a176638f2a3fe7704e9c000905bc0735e25ddc29
Nara Avila <nara.moraes@usp.br>
2026-02-26 19:51:42 -0300

    Update files

--- infra/main.tf
+++ infra/main.tf
@@ -74,7 +74,7 @@ resource "aws_lambda_function" "handler" {
   filename         = data.archive_file.handler_zip.output_path
   source_code_hash = data.archive_file.handler_zip.output_base64sha256
 
-  memory_size = 128
+  memory_size = 512
   timeout     = 15
 
   environment {
