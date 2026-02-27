83064dee334b38fc2b1cafaf733a373e787470f2
Nara Avila <nara.moraes@usp.br>
2026-02-26 21:24:50 -0300

    Update files

--- infra/lambda_src/handler/main_app/main_controller.py
+++ infra/lambda_src/handler/main_app/main_controller.py
@@ -11,6 +11,7 @@ from engine.engine_symmetry_analyser import SymmetryAnalyzer
 from representation.representation_type import RepresentationType
 from analysis.analise_tipo import AnaliseTipo
 from render.render_tipo import RenderTipo
+from render.render_pdf import PdfReportGenerator
 
 # from pymatgen.core.structure import Molecule as PymatgenMolecule
 # from pymatgen.symmetry.analyzer import PointGroupAnalyzer
