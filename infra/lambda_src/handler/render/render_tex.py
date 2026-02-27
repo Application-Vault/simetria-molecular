122cd52d18e4fd5ff2563522c2f963598e830800
Nara Avila <nara.moraes@usp.br>
2026-02-26 22:10:42 -0300

    Update files

--- infra/lambda_src/handler/render/render_tex.py
+++ infra/lambda_src/handler/render/render_tex.py
@@ -16,8 +16,8 @@
 """
 
 # latex_generator.py
-from datetime import datetime
 import re
+
 
 class LatexReportGenerator:
     def __init__(self, metadata, resultado):
@@ -28,214 +28,259 @@ class LatexReportGenerator:
         blocos = []
 
         if "permutacoes" in self.resultado:
-            blocos.append(r"""
-                \section{Permutações Básicas}
-                \[
-                %s
-                \]
-                """ % self._formatar_permutacoes(self.resultado['permutacoes']))
+            blocos.append(
+                r"""
+\section{Permutações Básicas}
+\[
+%s
+\]
+"""
+                % self._formatar_permutacoes(self.resultado["permutacoes"])
+            )
 
         if "operacoes_multiplicacao" in self.resultado:
-            blocos.append(r"""
-                \section{Operações de Multiplicação}
-                \subsection{Tabela de Multiplicação}
-                \begin{adjustbox}{max width=\linewidth, angle=90, center}
-                \[
-                %s
-                \]
-                \end{adjustbox}
-                """ % self._formatar_tabela_multiplicacao(self.resultado['operacoes_multiplicacao']))
+            blocos.append(
+                r"""
+\section{Operações de Multiplicação}
+\subsection{Tabela de Multiplicação}
+\begin{adjustbox}{max width=\linewidth, angle=90, center}
+\[
+%s
+\]
+\end{adjustbox}
+"""
+                % self._formatar_tabela_multiplicacao(self.resultado["operacoes_multiplicacao"])
+            )
 
-            blocos.append(r"""
-                \subsection{Operações de Multiplicação Detalhadas}
-                \begin{longtable}{>{$}r<{$} >{$}l<{$} >{$}l<{$}}
-                %s
-                \end{longtable}
-                """ % self._formatar_operacoes_multiplicacao(self.resultado['operacoes_multiplicacao']))
+            blocos.append(
+                r"""
+\subsection{Operações de Multiplicação Detalhadas}
+\begin{longtable}{>{$}r<{$} >{$}l<{$} >{$}l<{$}}
+%s
+\end{longtable}
+"""
+                % self._formatar_operacoes_multiplicacao(self.resultado["operacoes_multiplicacao"])
+            )
 
         if "operacoes_conjugacao" in self.resultado:
             classes = self._extrair_classes_de_operacoes(self.resultado["operacoes_conjugacao"])
 
-            blocos.append(r"""
-                \section{Operações de Conjugação}
-                \subsection{Tabela de Conjugação}
-                \begin{adjustbox}{max width=\linewidth, angle=90, center}
-                \begin{longtable}{l}
-                %s
-                \end{longtable}
-                \end{adjustbox}
-                """ % self._formatar_tabela_conjugacao(self.resultado["operacoes_conjugacao"]))
+            blocos.append(
+                r"""
+\section{Operações de Conjugação}
+\subsection{Tabela de Conjugação}
+\begin{adjustbox}{max width=\linewidth, angle=90, center}
+\[
+%s
+\]
+\end{adjustbox}
+"""
+                % self._formatar_tabela_conjugacao(self.resultado["operacoes_conjugacao"])
+            )
 
-            blocos.append(r"""
-                \subsection{Operações de Conjugação Detalhadas}
-                %s
-                """ % self._formatar_operacoes_conjugacao(self.resultado["operacoes_conjugacao"]))
+            blocos.append(
+                r"""
+\subsection{Operações de Conjugação Detalhadas}
+%s
+"""
+                % self._formatar_operacoes_conjugacao(self.resultado["operacoes_conjugacao"])
+            )
 
-            blocos.append(r"""
-                \subsection{Agrupamento em Classes de Conjugação}
-                \begin{itemize}
-                %s
-                \end{itemize}
-                """ % "\n".join(
-                    f"\\item \\textbf{{{classe}}}: {', '.join(f'${op}$' for op in ops)}"
+            # Aqui eu deixo como TEXTO (não $...$), porque os nomes podem ter "_" etc.
+            # Se quiser matemático, dá pra trocar por \ensuremath{...} com escape cuidadoso.
+            blocos.append(
+                r"""
+\subsection{Agrupamento em Classes de Conjugação}
+\begin{itemize}
+%s
+\end{itemize}
+"""
+                % "\n".join(
+                    rf"\item \textbf{{{self._latex_escape_text(classe)}}}: "
+                    + ", ".join(rf"\texttt{{{self._latex_escape_text(op)}}}" for op in ops)
                     for classe, ops in classes.items()
-                ))
+                )
+            )
+
+        # metadados em texto (podem ter underscore etc.)
+        sistema = self._latex_escape_text(str(self.metadata.get("sistema", "")))
+        molecula = self._latex_escape_text(str(self.metadata.get("molecula", "")))
+        grupo = self._latex_escape_text(str(self.metadata.get("grupo", "")))
+        ordem = self._latex_escape_text(str(self.metadata.get("ordem", "")))
+        data = self._latex_escape_text(str(self.metadata.get("data", "")))
+        uuid = self._latex_escape_text(str(self.metadata.get("uuid", "")))
+        tempo_exec = self._latex_escape_text(str(self.resultado.get("tempo_execucao", "")))
 
         return fr"""\documentclass[a4paper,12pt]{{article}}
-        \usepackage{{datetime2}}
-        \usepackage{{graphicx}}
-        \usepackage[utf8]{{inputenc}}
-        \usepackage{{fancyhdr}}
-        \usepackage{{geometry}}
-        \usepackage{{amsmath}}
-        \usepackage{{longtable}}
-        \usepackage{{lastpage}}
-        \usepackage[hidelinks]{{hyperref}}
-        \usepackage{{adjustbox}}
-        \usepackage{{lscape}}  % ou rotfloat, caso queira controlar melhor
+\usepackage{{datetime2}}
+\usepackage{{graphicx}}
+\usepackage[utf8]{{inputenc}}
+\usepackage{{fancyhdr}}
+\usepackage{{geometry}}
+\usepackage{{amsmath}}
+\usepackage{{longtable}}
+\usepackage{{lastpage}}
+\usepackage[hidelinks]{{hyperref}}
+\usepackage{{adjustbox}}
+\usepackage{{lscape}}
 
-        \geometry{{left=1cm, right=1cm, top=2.5cm, bottom=2.5cm}}
-        \pagestyle{{fancy}}
-        \fancyhf{{}}
+\geometry{{left=1cm, right=1cm, top=2.5cm, bottom=2.5cm}}
+\pagestyle{{fancy}}
+\fancyhf{{}}
 
-        \setlength{{\headheight}}{{35pt}}  % altura reservada para o cabeçalho
-        \setlength{{\headsep}}{{2em}}      % espaço entre cabeçalho e corpo
+% fancyhdr estava avisando headheight; já deixo o recomendado:
+\setlength{{\headheight}}{{42pt}}
+\setlength{{\headsep}}{{2em}}
 
-        % Cabeçalho
-        \fancyhead[L]{{\textbf{{Análise de Simetria}} \\
-        \textbf{{Sistema {self.metadata['sistema']}}} \\
-        Tempo de Execução: \textbf{{{self.resultado['tempo_execucao']}}}}}
+% Cabeçalho (texto)
+\fancyhead[L]{{\textbf{{Análise de Simetria}} \\
+\textbf{{Sistema {sistema}}} \\
+Tempo de Execução: \textbf{{{tempo_exec}}}}}
 
-        \fancyhead[C]{{Molécula: \textbf{{{self.metadata['molecula']}}} \\
-        Grupo: \textbf{{{self.metadata['grupo']}}} \\
-        Ordem: \textbf{{{self.metadata['ordem']}}}}}
+\fancyhead[C]{{Molécula: \textbf{{{molecula}}} \\
+Grupo: \textbf{{{grupo}}} \\
+Ordem: \textbf{{{ordem}}}}}
 
-        \fancyhead[R]{{{self.metadata['data']} \\
-        Relatório: \textbf{{{self.metadata['uuid']}}} \\
-        Página \thepage\ de \pageref{{LastPage}}}}
+\fancyhead[R]{{{data} \\
+Relatório: \textbf{{{uuid}}} \\
+Página \thepage\ de \pageref{{LastPage}}}}
 
-        \fancyfoot[L]{{\scriptsize {{\tiny \textcopyright}} naavilam.github.io/simetria-molecular contact@chanah.dev}}
+\fancyfoot[L]{{\scriptsize {{\tiny \textcopyright}} naavilam.github.io/simetria-molecular contact@chanah.dev}}
 
-        \begin{{document}}
+\begin{{document}}
 
-        \tableofcontents
-        \newpage
+\tableofcontents
+\newpage
 
-        {''.join(blocos)}
-        \end{{document}}
-        """
+{''.join(blocos)}
+\end{{document}}
+"""
+
+    # -------------------------
+    # FORMATADORES
+    # -------------------------
 
     def _formatar_permutacoes(self, permutacoes: dict) -> str:
-        linhas = ["\\begin{array}{r@{\\,:\\ }l}"]
+        linhas = [r"\begin{array}{r@{\,:\ }l}"]
         for nome, lista in permutacoes.items():
-            linhas.append(f"\\mathrm{{{nome}}} & {lista} \\\\")
-        linhas.append("\\end{array}")
+            nome_tex = rf"\text{{{self._latex_escape_text(str(nome))}}}"
+            lista_tex = rf"\text{{{self._latex_escape_text(str(lista))}}}"
+            linhas.append(rf"{nome_tex} & {lista_tex} \\")
+        linhas.append(r"\end{array}")
         return "\n".join(linhas)
 
     def _formatar_tabela_multiplicacao(self, tabela: dict) -> str:
         chaves = list(tabela.keys())
         linhas = []
 
-        linhas.append("\\begin{array}{c|" + "c" * len(chaves) + "}")
-        linhas.append(" & " + " & ".join(f"{op}" for op in chaves) + r" \\ \hline")
+        linhas.append(r"\begin{array}{c|" + ("c" * len(chaves)) + r"}")
+
+        header = " & " + " & ".join(rf"\text{{{self._latex_escape_text(str(op))}}}" for op in chaves) + r" \\ \hline"
+        linhas.append(header)
 
         for op1, linha in tabela.items():
             valores = [linha[op2]["nome"] for op2 in chaves]
-            linhas.append(f"{op1} & " + " & ".join(valores) + r" \\")
 
-        linhas.append("\\end{array}")
+            op1_tex = rf"\text{{{self._latex_escape_text(str(op1))}}}"
+            valores_tex = " & ".join(rf"\text{{{self._latex_escape_text(str(v))}}}" for v in valores)
+
+            linhas.append(op1_tex + " & " + valores_tex + r" \\")
+
+        linhas.append(r"\end{array}")
         return "\n".join(linhas)
 
     def _formatar_operacoes_multiplicacao(self, operacoes: dict) -> str:
         linhas = []
         for op1, linha in operacoes.items():
             for op2, info in linha.items():
-                perm2 = info["permutacao_op2"]
-                perm_result = info["permutacao_resultante"]
-                resultado = info["nome"]
+                perm2 = str(info.get("permutacao_op2", ""))
+                perm_result = str(info.get("permutacao_resultante", ""))
+                resultado = str(info.get("nome", ""))
+
+                op1e = self._latex_escape_text(str(op1))
+                op2e = self._latex_escape_text(str(op2))
+
+                # permutações/resultados podem conter caracteres especiais, então escapamos como texto
+                perm2e = self._latex_escape_text(perm2)
+                permrese = self._latex_escape_text(perm_result)
+                rese = self._latex_escape_text(resultado)
+
                 linhas.append(
-                    rf"\makebox[3.3cm][r]{{$\mathrm{{{op1}}} \circ \mathrm{{{op2}}}$}} &="
-                    rf" $\mathrm{{{op1}}} \circ {perm2}$ &="
-                    rf" ${perm_result}$ &="
-                    rf" \makebox[2.3cm][l]{{$\mathrm{{{resultado}}}$}} \\"
+                    rf"\makebox[3.3cm][r]{{$\mathrm{{{op1e}}} \circ \mathrm{{{op2e}}}$}} &="
+                    rf" \text{{{op1e}}} \circ \text{{{perm2e}}} &="
+                    rf" \text{{{permrese}}} &="
+                    rf" \makebox[2.3cm][l]{{\text{{{rese}}}}} \\"
                 )
         return "\n".join(linhas)
 
     def _formatar_operacoes_conjugacao(self, operacoes: dict) -> str:
         linhas = []
         for g, conjugacoes in operacoes.items():
-            linhas.append(f"\\subsection*{{Conjugações de ${g}$}}")
+            ge = self._latex_escape_text(str(g))
+            linhas.append(rf"\subsection*{{Conjugações de \texttt{{{ge}}}}}")
+
             for h, info in conjugacoes.items():
-                g_perm = info['detalhe']['g']
-                h_perm = info['detalhe']['h']
-                hgh_inv = info['detalhe']['hgh⁻¹']
-                res = info['resultado']
+                he = self._latex_escape_text(str(h))
+                detalhe = info.get("detalhe", {}) or {}
+
+                # cuidado com a chave hgh⁻¹ (tem caractere unicode)
+                hgh_inv = detalhe.get("hgh⁻¹") or detalhe.get("hgh-1") or detalhe.get("hgh^-1") or ""
+
+                hgh_e = self._latex_escape_text(str(hgh_inv))
+                res_e = self._latex_escape_text(str(info.get("resultado", "")))
+
+                # Tudo como texto para não quebrar com underscores etc.
                 linhas.append(
-                    rf"$\mathrm{{{h}}} \circ \mathrm{{{g}}} \circ \mathrm{{{h}}}^{{-1}} = {hgh_inv} = \mathrm{{{res}}}$ \\"
+                    rf"\texttt{{{he}}} $\circ$ \texttt{{{ge}}} $\circ$ \texttt{{{he}}}^{{-1}} "
+                    rf"$=$ \texttt{{{hgh_e}}} $=$ \texttt{{{res_e}}} \\"
                 )
         return "\n".join(linhas)
-
-    @staticmethod
-    def _extrair_classes_de_operacoes(operacoes: dict) -> dict:
-        """
-        Agrupa elementos conjugados em classes disjuntas.
-        Cada par (g, resultado) da conjugação entra no mesmo conjunto.
-        """
-        nomes = sorted({g for g in operacoes} |
-                       {h for conj in operacoes.values() for h in conj} |
-                       {info["resultado"] for conj in operacoes.values() for info in conj.values()})
-
-        grupos = []
-
-        def encontrar_grupo(elem):
-            for grupo in grupos:
-                if elem in grupo:
-                    return grupo
-            return None
-
-        for g, conjugacoes in operacoes.items():
-            for h, info in conjugacoes.items():
-                res = info["resultado"]
-
-                grupo_g = encontrar_grupo(g)
-                grupo_res = encontrar_grupo(res)
-
-                if grupo_g and grupo_res:
-                    if grupo_g is not grupo_res:
-                        grupo_g.update(grupo_res)
-                        grupos.remove(grupo_res)
-                elif grupo_g:
-                    grupo_g.add(res)
-                elif grupo_res:
-                    grupo_res.add(g)
-                else:
-                    grupos.append(set([g, res]))
-
-        return {
-            f"Classe {i+1}": sorted(grupo, key=nomes.index)
-            for i, grupo in enumerate(grupos)
-        }
-
 
     def _formatar_tabela_conjugacao(self, operacoes: dict) -> str:
         nomes = list(operacoes.keys())
         linhas = []
 
-        linhas.append("\\begin{array}{c|" + "c" * len(nomes) + "}")
-        linhas.append(" & " + " & ".join(nomes) + " \\\\ \\hline")
+        linhas.append(r"\begin{array}{c|" + ("c" * len(nomes)) + r"}")
+        linhas.append(
+            " & " + " & ".join(rf"\text{{{self._latex_escape_text(str(n))}}}" for n in nomes) + r" \\ \hline"
+        )
 
         for g, resultados in operacoes.items():
-            linha = [g]
+            row = [rf"\text{{{self._latex_escape_text(str(g))}}}"]
             for h in nomes:
-                linha.append(resultados[h]['resultado'])
-            linhas.append(" & ".join(linha) + " \\\\")
-        linhas.append("\\end{array}")
+                val = (resultados.get(h) or {}).get("resultado", "")
+                row.append(rf"\text{{{self._latex_escape_text(str(val))}}}")
+            linhas.append(" & ".join(row) + r" \\")
+        linhas.append(r"\end{array}")
         return "\n".join(linhas)
+
+    # -------------------------
+    # HELPERS
+    # -------------------------
 
     @staticmethod
     def latex_safe(op: str) -> str:
         """
         Corrige superscript/subscript duplos como \mathrm{C}_{2}^{(a)} para evitar erro de compilação LaTeX.
         """
-        return re.sub(r"(\\mathrm\{[A-Za-z]+\})_\{([^\}]+)\}\^\{([^\}]+)\}",
-                      r"\1_{\2}^{\3}", op)
+        return re.sub(
+            r"(\\mathrm\{[A-Za-z]+\})_\{([^\}]+)\}\^\{([^\}]+)\}",
+            r"\1_{\2}^{\3}",
+            op,
+        )
+
+    def _latex_escape_text(self, s: str) -> str:
+        # escape bem conservador pra TEX (modo texto via \text{} / \texttt{})
+        return (
+            str(s)
+            .replace("\\", r"\textbackslash{}")
+            .replace("&", r"\&")
+            .replace("%", r"\%")
+            .replace("$", r"\$")
+            .replace("#", r"\#")
+            .replace("_", r"\_")
+            .replace("{", r"\{")
+            .replace("}", r"\}")
+            .replace("~", r"\textasciitilde{}")
+            .replace("^", r"\textasciicircum{}")
+        )
