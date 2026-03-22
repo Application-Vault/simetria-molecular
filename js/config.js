1f7e52f509fbb2a27add85b5c23c93fb91582cae
Nara Avila <nara.moraes@usp.br>
2026-03-21 21:26:48 -0300

    Update files

--- js/config.js
+++ js/config.js
@@ -2,6 +2,5 @@ export const API_CONFIG = {
   baseUrlAnalise: 'https://x8clyvj53d.execute-api.us-east-2.amazonaws.com/api/analise',
   baseUrlGrupos: 'https://x8clyvj53d.execute-api.us-east-2.amazonaws.com/api/grupo/',
   baseUrlMoleculas: 'https://x8clyvj53d.execute-api.us-east-2.amazonaws.com/api/molecula/',
-  // Preencha depois quando o Lambda de PDF estiver pronto
-  baseUrlPdf: ''
+  baseUrlPdf: "https://x8clyvj53d.execute-api.us-east-2.amazonaws.com/api/pdf",
 };
